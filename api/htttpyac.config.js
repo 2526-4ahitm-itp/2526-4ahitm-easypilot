d22b8a4e5998c89124402459ae43fa31ba020174
simonneder <s.eder1@htblaleonding.onmicrosoft.com>
2026-01-20 08:52:44 +0100

    httpyac config added

---
+++ api/htttpyac.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    environments :{
+        $shared:{
+            host: '',
+            version: 'v1'
+        },
+        dev: {
+            host: 'http://localhost:8080'
+        }
+    }
+}
