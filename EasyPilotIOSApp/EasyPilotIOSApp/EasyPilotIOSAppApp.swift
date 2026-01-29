7e74670b3ee7421615d8ef27a13ef73051efc255
Eder Simon <it220234@WS8A20.htl-leonding.ac.at>
2026-01-29 14:08:23 +0100

    initial commit of EasyPilotIOSApp

---
+++ EasyPilotIOSApp/EasyPilotIOSApp/EasyPilotIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPilotIOSAppApp.swift
+//  EasyPilotIOSApp
+//
+//  Created by Eder Simon on 29.01.26.
+//
+
+import SwiftUI
+
+@main
+struct EasyPilotIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyPilotIOSApp/EasyPilotIOSAppTests/EasyPilotIOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPilotIOSAppTests.swift
+//  EasyPilotIOSAppTests
+//
+//  Created by Eder Simon on 29.01.26.
+//
+
+import Testing
+@testable import EasyPilotIOSApp
+
+struct EasyPilotIOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
